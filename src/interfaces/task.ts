5f38de0adaec647b0b3e60260ba86c5a13e61367
root <shlomiko@rafael.co.il>
2023-06-11 17:13:09 +0300

    fix: artifact epsg code

--- src/interfaces/task.ts
+++ src/interfaces/task.ts
@@ -1,12 +1,12 @@
 import { FeatureCollection, MultiPolygon, Polygon } from '@turf/turf';
-import { Epsg } from '@map-colonies/types';
+import { EpsgCode } from '@map-colonies/types';
 import { Webhook } from '../interfaces/webhook';
 import { GeometryMetadata } from './exportManager';
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface CreateExportTaskRequest<T> {
   catalogRecordID: string;
-  artifactCRS: Epsg;
+  artifactCRS: EpsgCode;
   webhook: Webhook[];
   ROI?: FeatureCollection;
   parameters?: T;
