90d0d0b258a4717970dae5032f5343638294b4e1
SHLOMIKO <shlomiko@rafael.co.il>
2023-09-19 17:22:14 +0300

    fix: estimation params

--- src/interfaces/exportManager.ts
+++ src/interfaces/exportManager.ts
@@ -6,10 +6,15 @@ export interface GetEstimationsResponse {
   estimatedTime?: number;
 }
 
+export interface RasterEstParams {}
+export interface DemEstParams {}
+export interface EstParams3D {}
+
 export declare type GeometryMetadata = Record<string, unknown>;
+export declare type EstimationsParams = RasterEstParams | DemEstParams | EstParams3D;
 
 export interface IExportManager {
   createExportTask: (data: CreateExportTaskRequest<TaskParameters>) => Promise<CreateExportTaskResponse>;
-  getEstimations: (catalogRecordID: string, ROI: FeatureCollection, additional?: Record<string, unknown>) => Promise<GetEstimationsResponse>;
+  getEstimations: (catalogRecordID: string, ROI: FeatureCollection, additional?: EstParams3D) => Promise<GetEstimationsResponse>;
   getFootprint: (catalogRecordID: string) => Promise<FeatureCollection>;
 }
